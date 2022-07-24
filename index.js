c5715737aff570d0bfe7bff07a31521fc1cdaffd
atcolab <s13tc2@protonmail.com>
2022-07-24 18:07:23 -0400

    add mongo db

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Note = require('./models/note')
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
-
-
-let notes = [
-    { 
-        id: 1, 
-        content: "HTML is easy", 
-        date: "2022-05-30T17:30:31.098Z", 
-        important: true 
-    }, 
-    { 
-        id: 2, 
-        content: "Browser can execute only Javascript", 
-        date: "2022-05-30T18:39:34.091Z", 
-        important: false 
-    }, 
-    { 
-        id: 3, 
-        content: "GET and POST are the most important methods of HTTP protocol", 
-        date: "2022-05-30T19:20:14.298Z", 
-        important: true 
-    }
-]
 
 // const app = http.createServer((request, response) => {
 //     response.writeHead(200, { 'Content-Type': 'application/json' })
@@ -38,7 +18,9 @@ app.get('/', (request, response) => {
 })
   
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -87,6 +69,6 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
